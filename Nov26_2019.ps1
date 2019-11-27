5feb75c3660ad4c8fe63ca7cdaa23403ea95f4bf
sunny <sunnyjamwal@gmail.com>
2019-11-26 21:44:58 -0500

    Initial commit

---
+++ Nov26_2019.ps1
@@ -0,0 +1,6 @@
+#PowerShell to calculate Mean and Standard Deviation
+#
+#More information on Standard Deviation : https://www.mathsisfun.com/data/standard-deviation.html
+
+
+1..10 | Measure-Object -Average -StandardDeviation

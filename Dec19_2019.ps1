037f567780390bbec050b748cc152d023725aa38
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2019-12-19 19:46:08 -0400

    Create Dec19_2019.ps1

---
+++ Dec19_2019.ps1
@@ -0,0 +1,9 @@
+#powerShell 101 : Split Operator
+
+"Merry Christmas HOHO" -split " " #Space is our deliminator
+
+"`n"
+
+"Where; is;             Waldo" -split   ";"  #In this case ";" is the deliminator
+
+"`n"

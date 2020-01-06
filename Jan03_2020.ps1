85d8db86e2c07430725dd669c35cf0981214a2b1
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2020-01-06 15:25:05 -0400

    Initial commit

--- Jan03_2020.ps1
+++ Jan03_2020.ps1
@@ -1,5 +1,3 @@
 #PowerShell one liner to start a porcess using WMI
 
 Invoke-CimMethod -ClassName Win32_Process -MethodName "Create" -Arguments @{ CommandLine = "cmd.exe" }
-
-(New-CimInstance -ClassName Win32_Process -ClientOnly).Create("cmd.exe")
---
+++ Jan06_2020.ps1
@@ -0,0 +1,3 @@
+#PowerShell one liner to get list of installed programs 
+
+Get-Package -Provider Programs -IncludeWindowsInstaller
